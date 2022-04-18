39685a7de2aac5c2b96de603b1f03926b6c79987
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2022-04-17 23:35:49 -0300

    :sparkles: add arrasta e solta
    add função de arrasta e solta ao projeto.
    realizado também incremento do atributo ondragstart ao criar um elemento novo.

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ function addnewtask(){
   const tTask = window.prompt("Informe o nove da sua nova TASK: ");
   const cTask = window.prompt("Informe o conteúdo da sua TASK: ");
 
+//verificar se o id task ja existe e acrecentando um numero a frente
+  var noId = "#task";
+  var numId = 0;
+  if(numId == numId){
+    var resId = `${noId}` + `${numId}`;
+  }else{
+    var  resId = `${noId}` + `${++numId}`;
+}
+
   const novo_elemento = document.createElement('div'); //informando que o elemento será uma div
   novo_elemento.className = "tesk"; // atribuindo uma class para o elemento criado
-  novo_elemento.id = "task1"; // atribuindo um id para o elemento criado
+  novo_elemento.id = resId; // atribuindo um id para o elemento criado
   novo_elemento.draggable = "true"; //add o atributo draggable para o elemento 
-  novo_elemento.ondragstart; // ainda não descobrir como adribuir o ondragstart ao elemento
+  novo_elemento.setAttribute("ondragstart", "drag(event)"); // add atributo de Ondragstart no elemento criado 
   novo_elemento.innerHTML = `${tTask}<br>${cTask}`; 
 
   const elemento = document.querySelector(".blocoContent"); // selecionando onde o elemento será criado
@@ -15,19 +24,17 @@ function addnewtask(){
 
 };
 
-/** 
+//função de arrastar e soltar o elemento criado
 function allowDrop(ev){
   ev.preventDefault();
 }
 
 function drag(ev){
-  ev.dataTransfer.setData("text/plain", ev.target.id);
+  ev.dataTransfer.setData("text/plain", ev.target.id); //verifica o tipo de dados e o valor dos dados arrastados
 }
 
 function drop(ev){
   ev.preventDefault();
   const data = ev.dataTransfer.getData("text/plain");
-  ev.target.appendChild(document.getElementById(data));
+  ev.target.appendChild(document.getElementById(data)); // quando dos dados arrastados são soltos, ocorre um novo evento de saltar
 }
-
-*/
